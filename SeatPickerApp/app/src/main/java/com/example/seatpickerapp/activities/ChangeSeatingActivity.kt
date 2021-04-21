3e47906276a4f643f9fa6c108feaf0ad824431fb
Malachy Keaveny <c16775539@mytudublin.ie>
2021-04-21 21:54:42 +0100

    ChangeSeatingActivity updated

--- SeatPickerApp/app/src/main/java/com/example/seatpickerapp/activities/ChangeSeatingActivity.kt
+++ SeatPickerApp/app/src/main/java/com/example/seatpickerapp/activities/ChangeSeatingActivity.kt
@@ -32,6 +32,7 @@ class ChangeSeatingActivity : AppCompatActivity() {
     private var auth: FirebaseAuth? = null
     private val personCollectionRef = Firebase.firestore.collection("users")
     private val db = FirebaseFirestore.getInstance()
+    private var adminCurrentRestaurant: String?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,24 +41,22 @@ class ChangeSeatingActivity : AppCompatActivity() {
         setContentView(view)
         auth = FirebaseAuth.getInstance()
 
+        adminCurrentRestaurant = AdminHomeActivity.adminRestaurantName.replace("\\s".toRegex(), "").decapitalize(Locale.ROOT)
+        Log.d("ChangeSeatingActivity", adminCurrentRestaurant!!)
+
         binding.dateBtn.setOnClickListener {
             setDate()
         }
 
-        binding.defaultBtn.setOnClickListener {
-            setDefaultTablesLayout()
+        binding.twoMetreCardView.setOnClickListener {
             tableLayout = "default"
         }
 
-        binding.twoSeaterBtn.setOnClickListener {
-            setTwoSeaterLayout()
-            tableLayout = "twoSeater"
+        binding.fourMetreCardView.setOnClickListener {
+            tableLayout = "fourMetre"
         }
 
-        binding.fourSeaterBtn.setOnClickListener {
-            setFourSeaterLayout()
-            tableLayout = "fourSeater"
-        }
+
 
         binding.confirmSeatChangeBtn.setOnClickListener {
             if (date == null || tableLayout == null) {
@@ -73,13 +72,93 @@ class ChangeSeatingActivity : AppCompatActivity() {
 
     }
 
+    private fun setFourMetreLayoutFlanagans() {
+        val tableLayoutDocRef = db.collection("restaurants").document("flanagans").collection("tableLayouts").document(date!!)
+        tableLayoutDocRef.get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+                    Toast.makeText(this, "A special layout already exists for $date !", Toast.LENGTH_SHORT).show()
+                    Log.d("ChangeSeatingActivity", "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d("ChangeSeatingActivity", "No such document")
+
+                    val layoutHashMap = hashMapOf(
+                        "date" to date,
+                        "tableLayout" to tableLayout,
+                        "tableOneVisible" to true,
+                        "tableTwoVisible" to false,
+                        "tableThreeVisible" to true,
+                        "tableFourVisible" to true,
+                        "tableFiveVisible" to false,
+                        "tableSixVisible" to false,
+                        "tableSevenVisible" to false,
+                        "tableEightVisible" to true,
+                    )
+
+                   tableLayoutDocRef.set(layoutHashMap).addOnSuccessListener {
+                       Log.d("seatingChangeConfirm", "Layout change successfully written!")
+                       Toast.makeText(this@ChangeSeatingActivity, "Layout change successfully written!", Toast.LENGTH_SHORT).show()
+                   }.addOnFailureListener { e ->
+                       Log.w("seatingChangeConfirmF", "Error writing document", e)
+                       Toast.makeText(this@ChangeSeatingActivity, e.toString(), Toast.LENGTH_SHORT).show()
+                   }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("ChangeSeatingActivity", "get failed with ", exception)
+            }
+    }
+
+    private fun setFourMetreLayoutOakFirePizza() {
+        val tableLayoutDocRef = db.collection("restaurants").document("oakFirePizza").collection("tableLayouts").document(date!!)
+        tableLayoutDocRef.get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+                    Toast.makeText(this, "A special layout already exists for $date !", Toast.LENGTH_SHORT).show()
+                    Log.d("ChangeSeatingActivity", "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d("ChangeSeatingActivity", "No such document")
+
+                    val layoutHashMap = hashMapOf(
+                        "date" to date,
+                        "tableLayout" to tableLayout,
+                        "tableOneVisible" to true,
+                        "tableTwoVisible" to false,
+                        "tableThreeVisible" to true,
+                        "tableFourVisible" to false,
+                        "tableFiveVisible" to true,
+                        "tableSixVisible" to false,
+                        "tableSevenVisible" to true,
+                        "tableEightVisible" to false,
+                        "tableNineVisible" to true,
+                        "tableTenVisible" to false
+                    )
+
+                    tableLayoutDocRef.set(layoutHashMap).addOnSuccessListener {
+                        Log.d("seatingChangeConfirm", "Layout change successfully written!")
+                        Toast.makeText(this@ChangeSeatingActivity, "Layout change successfully written!", Toast.LENGTH_SHORT).show()
+                    }.addOnFailureListener { e ->
+                        Log.w("seatingChangeConfirmF", "Error writing document", e)
+                        Toast.makeText(this@ChangeSeatingActivity, e.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("ChangeSeatingActivity", "get failed with ", exception)
+            }
+    }
+
     private fun confirmLayoutDialog(date: String, tableLayout: String) {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Confirm table layout change")
-        builder.setMessage("Date: $date \nLayout: $tableLayout")
+        builder.setMessage("Restaurant: ${adminCurrentRestaurant}\nDate: $date \nLayout: $tableLayout")
 
         builder.setPositiveButton(android.R.string.yes) { dialog, which ->
-            createTableLayoutChange(date, tableLayout)
+            //createTableLayoutChange(date, tableLayout)
+            when (adminCurrentRestaurant) {
+                "flanagans" -> setFourMetreLayoutFlanagans()
+                "oakFirePizza" -> setFourMetreLayoutOakFirePizza()
+            }
         }
 
         builder.setNegativeButton(android.R.string.no) { dialog, which ->
@@ -126,221 +205,9 @@ class ChangeSeatingActivity : AppCompatActivity() {
             }
     }
 
-    private fun setDefaultTablesLayout() {
-        val dpRatio = applicationContext.resources.displayMetrics.density
-        val pixelForDP = 70 * dpRatio
 
-        Log.d("dpTest", "${70 * pixelForDP}")
-        val paramTableOne = binding.ivTableOne.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableTwo = binding.ivTableTwo.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableThree = binding.ivTableThree.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableFour = binding.ivTableFour.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableFive = binding.ivTableFive.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableSix = binding.ivTableSix.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableSeven = binding.ivTableSeven.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableEight = binding.ivTableEight.layoutParams as ViewGroup.MarginLayoutParams
 
-        paramTableOne.topMargin = (20 * dpRatio).toInt()
-        paramTableOne.leftMargin = (20 * dpRatio).toInt()
-        binding.ivTableOne.layoutParams = paramTableOne
-        binding.ivTableOne.scaleX = 1.25F
-        binding.ivTableOne.scaleY = 1.25F
 
-        paramTableTwo.topMargin = (170 * dpRatio).toInt()
-        paramTableTwo.leftMargin = (20 * dpRatio).toInt()
-        binding.ivTableTwo.layoutParams = paramTableTwo
-        binding.ivTableTwo.scaleX = 1.25F
-        binding.ivTableTwo.scaleY = 1.25F
-
-        paramTableThree.topMargin = (320 * dpRatio).toInt()
-        paramTableThree.leftMargin = (20 * dpRatio).toInt()
-        binding.ivTableThree.layoutParams = paramTableThree
-        binding.ivTableThree.scaleX = 1.25F
-        binding.ivTableThree.scaleY = 1.25F
-
-        paramTableFour.topMargin = (50 * dpRatio).toInt()
-        paramTableFour.leftMargin = (250 * dpRatio).toInt()
-        binding.ivTableFour.layoutParams = paramTableFour
-        binding.ivTableFour.scaleX = 1.5F
-        binding.ivTableFour.scaleY = 1.5F
-
-        paramTableFive.topMargin = (200 * dpRatio).toInt()
-        paramTableFive.leftMargin = (250 * dpRatio).toInt()
-        binding.ivTableFive.layoutParams = paramTableFive
-        binding.ivTableFive.scaleX = 1.5F
-        binding.ivTableFive.scaleY = 1.5F
-
-        paramTableSix.topMargin = (500 * dpRatio).toInt()
-        paramTableSix.leftMargin = (20 * dpRatio).toInt()
-        binding.ivTableSix.layoutParams = paramTableSix
-        binding.ivTableSix.scaleX = 1.5F
-        binding.ivTableSix.scaleY = 1.5F
-
-        paramTableSeven.topMargin = (450 * dpRatio).toInt()
-        paramTableSeven.leftMargin = (400 * dpRatio).toInt()
-        binding.ivTableSeven.layoutParams = paramTableSeven
-        binding.ivTableSeven.scaleX = 1.5F
-        binding.ivTableSeven.scaleY = 1.5F
-
-        paramTableEight.topMargin = (400 * dpRatio).toInt()
-        paramTableEight.leftMargin = (250 * dpRatio).toInt()
-        binding.ivTableEight.layoutParams = paramTableEight
-        binding.ivTableEight.scaleX = 1.5F
-        binding.ivTableEight.scaleY = 1.5F
-
-        binding.ivTableOne.setImageResource(R.drawable.ic_two_seater_light_blue)
-        binding.ivTableTwo.setImageResource(R.drawable.ic_two_seater_light_blue)
-        binding.ivTableThree.setImageResource(R.drawable.ic_two_seater_light_blue)
-        binding.ivTableFour.setImageResource(R.drawable.ic_six_seater_light_blue)
-        binding.ivTableFive.setImageResource(R.drawable.ic_six_seater_light_blue)
-        binding.ivTableSix.setImageResource(R.drawable.ic_four_seater_light_blue)
-        binding.ivTableSeven.setImageResource(R.drawable.ic_four_seater_light_blue)
-        binding.ivTableEight.setImageResource(R.drawable.ic_four_seater_light_blue)
-    }
-
-    private fun setTwoSeaterLayout() {
-        val dpRatio = applicationContext.resources.displayMetrics.density
-        val pixelForDP = 70 * dpRatio
-
-        Log.d("dpTest", "${70 * pixelForDP}")
-        val paramTableOne = binding.ivTableOne.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableTwo = binding.ivTableTwo.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableThree = binding.ivTableThree.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableFour = binding.ivTableFour.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableFive = binding.ivTableFive.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableSix = binding.ivTableSix.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableSeven = binding.ivTableSeven.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableEight = binding.ivTableEight.layoutParams as ViewGroup.MarginLayoutParams
-
-        paramTableOne.topMargin = (50 * dpRatio).toInt()
-        paramTableOne.leftMargin = (20 * dpRatio).toInt()
-        binding.ivTableOne.layoutParams = paramTableOne
-        binding.ivTableOne.scaleX = 1.25F
-        binding.ivTableOne.scaleY = 1.25F
-
-        paramTableTwo.topMargin = (250 * dpRatio).toInt()
-        paramTableTwo.leftMargin = (20 * dpRatio).toInt()
-        binding.ivTableTwo.layoutParams = paramTableTwo
-        binding.ivTableTwo.scaleX = 1.25F
-        binding.ivTableTwo.scaleY = 1.25F
-
-        paramTableThree.topMargin = (450 * dpRatio).toInt()
-        paramTableThree.leftMargin = (20 * dpRatio).toInt()
-        binding.ivTableThree.layoutParams = paramTableThree
-        binding.ivTableThree.scaleX = 1.25F
-        binding.ivTableThree.scaleY = 1.25F
-
-        paramTableFour.topMargin = (150 * dpRatio).toInt()
-        paramTableFour.leftMargin = (175 * dpRatio).toInt()
-        binding.ivTableFour.layoutParams = paramTableFour
-        binding.ivTableFour.scaleX = 1.25F
-        binding.ivTableFour.scaleY = 1.25F
-
-        paramTableFive.topMargin = (350 * dpRatio).toInt()
-        paramTableFive.leftMargin = (175 * dpRatio).toInt()
-        binding.ivTableFive.layoutParams = paramTableFive
-        binding.ivTableFive.scaleX = 1.25F
-        binding.ivTableFive.scaleY = 1.25F
-
-        paramTableSix.topMargin = (50 * dpRatio).toInt()
-        paramTableSix.leftMargin = (330 * dpRatio).toInt()
-        binding.ivTableSix.layoutParams = paramTableSix
-        binding.ivTableSix.scaleX = 1.25F
-        binding.ivTableSix.scaleY = 1.25F
-
-        paramTableSeven.topMargin = (250 * dpRatio).toInt()
-        paramTableSeven.leftMargin = (330 * dpRatio).toInt()
-        binding.ivTableSeven.layoutParams = paramTableSeven
-        binding.ivTableSeven.scaleX = 1.25F
-        binding.ivTableSeven.scaleY = 1.25F
-
-        paramTableEight.topMargin = (450 * dpRatio).toInt()
-        paramTableEight.leftMargin = (330 * dpRatio).toInt()
-        binding.ivTableEight.layoutParams = paramTableEight
-        binding.ivTableEight.scaleX = 1.25F
-        binding.ivTableEight.scaleY = 1.25F
-
-        binding.ivTableOne.setImageResource(R.drawable.ic_two_seater_light_blue)
-        binding.ivTableTwo.setImageResource(R.drawable.ic_two_seater_light_blue)
-        binding.ivTableThree.setImageResource(R.drawable.ic_two_seater_light_blue)
-        binding.ivTableFour.setImageResource(R.drawable.ic_two_seater_light_blue)
-        binding.ivTableFive.setImageResource(R.drawable.ic_two_seater_light_blue)
-        binding.ivTableSix.setImageResource(R.drawable.ic_two_seater_light_blue)
-        binding.ivTableSeven.setImageResource(R.drawable.ic_two_seater_light_blue)
-        binding.ivTableEight.setImageResource(R.drawable.ic_two_seater_light_blue)
-    }
-
-    private fun setFourSeaterLayout() {
-        val dpRatio = applicationContext.resources.displayMetrics.density
-        val pixelForDP = 70 * dpRatio
-
-        Log.d("dpTest", "${70 * pixelForDP}")
-        val paramTableOne = binding.ivTableOne.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableTwo = binding.ivTableTwo.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableThree = binding.ivTableThree.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableFour = binding.ivTableFour.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableFive = binding.ivTableFive.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableSix = binding.ivTableSix.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableSeven = binding.ivTableSeven.layoutParams as ViewGroup.MarginLayoutParams
-        val paramTableEight = binding.ivTableEight.layoutParams as ViewGroup.MarginLayoutParams
-
-        paramTableOne.topMargin = (50 * dpRatio).toInt()
-        paramTableOne.leftMargin = (20 * dpRatio).toInt()
-        binding.ivTableOne.layoutParams = paramTableOne
-        binding.ivTableOne.scaleX = 1.5F
-        binding.ivTableOne.scaleY = 1.5F
-
-        paramTableTwo.topMargin = (250 * dpRatio).toInt()
-        paramTableTwo.leftMargin = (20 * dpRatio).toInt()
-        binding.ivTableTwo.layoutParams = paramTableTwo
-        binding.ivTableTwo.scaleX = 1.5F
-        binding.ivTableTwo.scaleY = 1.5F
-
-        paramTableThree.topMargin = (450 * dpRatio).toInt()
-        paramTableThree.leftMargin = (20 * dpRatio).toInt()
-        binding.ivTableThree.layoutParams = paramTableThree
-        binding.ivTableThree.scaleX = 1.5F
-        binding.ivTableThree.scaleY = 1.5F
-
-        paramTableFour.topMargin = (150 * dpRatio).toInt()
-        paramTableFour.leftMargin = (175 * dpRatio).toInt()
-        binding.ivTableFour.layoutParams = paramTableFour
-        binding.ivTableFour.scaleX = 1.5F
-        binding.ivTableFour.scaleY = 1.5F
-
-        paramTableFive.topMargin = (350 * dpRatio).toInt()
-        paramTableFive.leftMargin = (175 * dpRatio).toInt()
-        binding.ivTableFive.layoutParams = paramTableFive
-        binding.ivTableFive.scaleX = 1.5F
-        binding.ivTableFive.scaleY = 1.5F
-
-        paramTableSix.topMargin = (50 * dpRatio).toInt()
-        paramTableSix.leftMargin = (330 * dpRatio).toInt()
-        binding.ivTableSix.layoutParams = paramTableSix
-        binding.ivTableSix.scaleX = 1.5F
-        binding.ivTableSix.scaleY = 1.5F
-
-        paramTableSeven.topMargin = (250 * dpRatio).toInt()
-        paramTableSeven.leftMargin = (330 * dpRatio).toInt()
-        binding.ivTableSeven.layoutParams = paramTableSeven
-        binding.ivTableSeven.scaleX = 1.5F
-        binding.ivTableSeven.scaleY = 1.5F
-
-        paramTableEight.topMargin = (450 * dpRatio).toInt()
-        paramTableEight.leftMargin = (330 * dpRatio).toInt()
-        binding.ivTableEight.layoutParams = paramTableEight
-        binding.ivTableEight.scaleX = 1.5F
-        binding.ivTableEight.scaleY = 1.5F
-
-        binding.ivTableOne.setImageResource(R.drawable.ic_four_seater_light_blue)
-        binding.ivTableTwo.setImageResource(R.drawable.ic_four_seater_light_blue)
-        binding.ivTableThree.setImageResource(R.drawable.ic_four_seater_light_blue)
-        binding.ivTableFour.setImageResource(R.drawable.ic_four_seater_light_blue)
-        binding.ivTableFive.setImageResource(R.drawable.ic_four_seater_light_blue)
-        binding.ivTableSix.setImageResource(R.drawable.ic_four_seater_light_blue)
-        binding.ivTableSeven.setImageResource(R.drawable.ic_four_seater_light_blue)
-        binding.ivTableEight.setImageResource(R.drawable.ic_four_seater_light_blue)
-    }
 
     private fun setDate() {
         val dpd =
